de13cded0f9548004ca9cbdf9dba62f88aaf2d03
Joris Wilson <jwilson5@stevens.edu>
2024-10-10 09:29:51 -0400

    cleaning files

--- Car Detection/RoostCarDect/RoostCarDect/main.py
+++ Car Detection/RoostCarDect/RoostCarDect/main.py
@@ -1,5 +1,5 @@
 # source - https://youtu.be/m5ituNiReEw
-
+# test push
 # Import libraries
 from PIL import Image
 import cv2
@@ -7,8 +7,8 @@ import numpy as np
 # import requests 
 
 # Reading image form url
-img = Image.open('./carpark.png')
-img = img.resize((450,250))
+img = Image.open('./carpark1.png')
+img = img.resize((450,250)) # ensure that any image is resized to a standard size
 #img.show() - test loading the image (load successful)
 
 img_array = np.array(img)
@@ -18,22 +18,22 @@ img_gray = cv2.cvtColor(img_array,cv2.COLOR_BGR2GRAY) # convert to gray scale
 #pil_image = Image.fromarray(img_gray)
 #pil_image.show() - Successful gray code image produced
 
-img_blur = cv2.GaussianBlur(img_gray,(5,5),0) # de noise
+img_blur = cv2.GaussianBlur(img_gray,(5,5),0) # de noise. 5,5 is the kernel size, 0 is the sigma value
 #pil_image = Image.fromarray(img_blur)
 #pil_image.show() - Successful de noise
 
-img_dilated = cv2.dilate(img_blur,np.ones((3,3)))
+img_dilated = cv2.dilate(img_blur,np.ones((3,3))) # dilate the image. 
 #pil_image = Image.fromarray(img_dilated)
 #pil_image.show() - checked
 
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)) # create a kernel. 
 final_img = cv2.morphologyEx(img_dilated, cv2.MORPH_CLOSE, kernel) 
 #pil_image = Image.fromarray(final_img)
 #pil_image.show() - checked
 
-cascade_source = './cars.xml'
+cascade_source = './cars.xml' # source of the cascade file. is a pre trained model, can be changed if results are not good
 car_cascade = cv2.CascadeClassifier(cascade_source)
-cars = car_cascade.detectMultiScale(final_img, 1.09, 1) # detects any size of car
+cars = car_cascade.detectMultiScale(final_img, 1.01, 1) # detects any size of car. Scale factor, min neighbours
 print(len(cars))
 
 
